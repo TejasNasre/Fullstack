9c9e40773ccf8fc842fffb9e586dd3ff260f2a35
tejasnasre <tejasnasre120@gmail.com>
2024-02-19 13:12:55 +0530

    Chamge target

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server : {
-    proxy : {
-      '/api' : 'https://backend-sample-lw2c.onrender.com'
-    }
+    proxy: {
+      '/api': {
+        target: 'https://backend-sample-lw2c.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   plugins: [react()],
 })
